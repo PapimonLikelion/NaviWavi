395c3f8c63d83b10a0ee2db31fbec6fc686c0996
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2020-12-15 01:46:04 +0900

    feat: 목적지 검색 후 옵션 고르기 구현

--- app/src/main/java/com/example/naviwavi/FirstPageActivity.java
+++ app/src/main/java/com/example/naviwavi/FirstPageActivity.java
@@ -25,6 +25,12 @@ import javax.xml.parsers.ParserConfigurationException;
 
 public class FirstPageActivity extends AppCompatActivity {
     private static String appkey = "l7xx7605cdc317c24677991d6d78a182ff5f\n"; //앱키
+    //검색 후 유저가 클릭한 POI번호 저장
+    public static int userChosenOption;
+    //검색 후 유저가 클릭한 최종목표지 저장
+    public static String finalDestination;
+
+    public ArrayList<TMapPOIItem> destinationPOI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,39 +38,48 @@ public class FirstPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first_page);
     }
 
-    /*도착 지점 입력 후 엔터 누르면 해당 지점 경로를 위한 도착지점으로 지정*/
+    /*도착 지점 입력 후 후보지 검색*/
     public void endEnter(View v) throws ParserConfigurationException, SAXException, IOException {
-        EditText endPoint = (EditText) findViewById(R.id.endPoint);
-        String destination = endPoint.getText().toString();
+        EditText searchInput = (EditText) findViewById(R.id.endPoint);
+        String destination = searchInput.getText().toString();
         if (destination.equals("")) {
             Toast.makeText(getApplicationContext(), "도착지를 입력해주세요", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(getApplicationContext(), destination, Toast.LENGTH_SHORT).show();
-            TMapTapi tmaptapi = new TMapTapi(this);
-            tmaptapi.setSKTMapAuthentication (appkey);
-            /* 비동기 처리하지 않고 우선 구현 */
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            /* 사용자가 검색한 출발점, 도착점 위도 경도 받아오기 */
-            TMapData tmapdata = new TMapData();
-            ArrayList<TMapPOIItem> destinationPOI;
-            destinationPOI = tmapdata.findAllPOI(destination);
-            if (destinationPOI.size() != 0) {
-                Intent option = new Intent(this, OptionActivity.class);
-                option.putExtra("oneName", destinationPOI.get(0).name);
-                option.putExtra("oneRoadName", destinationPOI.get(0).roadName);
-                option.putExtra("twoName", destinationPOI.get(1).name);
-                option.putExtra("twoRoadName", destinationPOI.get(1).roadName);
-                option.putExtra("threeName", destinationPOI.get(2).name);
-                option.putExtra("threeRoadName", destinationPOI.get(2).roadName);
-                startActivity(option);
-            } else {
-                Toast.makeText(getApplicationContext(), "검색 결과가 없습니다", Toast.LENGTH_SHORT).show();
-            }
+            searchPossibleOption(destination);
         }
     }
 
-    /* 도착 위도 경도 정보 MainActivity로 넘겨줌 */
+    /* 후보지 1,2,3을 뽑아 FirstPageActivity에 전달 */
+    private void searchPossibleOption(String destination) throws ParserConfigurationException, SAXException, IOException {
+        Toast.makeText(getApplicationContext(), destination + "을 검색합니다", Toast.LENGTH_SHORT).show();
+        TMapTapi tmaptapi = new TMapTapi(this);
+        tmaptapi.setSKTMapAuthentication (appkey);
+        /* 비동기 처리하지 않고 우선 구현 */
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        /* 사용자가 검색한 출발점, 도착점 위도 경도 받아오기 */
+        TMapData tmapdata = new TMapData();
+
+        destinationPOI = tmapdata.findAllPOI(destination);
+        sendToOptionActivity();
+    }
+
+    private void sendToOptionActivity () {
+        if(destinationPOI != null) {
+            Intent option = new Intent(this, OptionActivity.class);
+            option.putExtra("oneName", destinationPOI.get(0).name);
+            option.putExtra("oneRoadName", destinationPOI.get(0).roadName);
+            option.putExtra("twoName", destinationPOI.get(1).name);
+            option.putExtra("twoRoadName", destinationPOI.get(1).roadName);
+            option.putExtra("threeName", destinationPOI.get(2).name);
+            option.putExtra("threeRoadName", destinationPOI.get(2).roadName);
+            startActivity(option);
+        } else {
+            Toast.makeText(getApplicationContext(), "검색 결과가 없습니다", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* 도착 지점의 위도 경도 정보를 MainActivity로 넘겨줌 */
     public void startNavigation(View v) throws ParserConfigurationException, SAXException, IOException {
         EditText Destination = (EditText) findViewById(R.id.endPoint);
         String destination = Destination.getText().toString();
@@ -74,17 +89,10 @@ public class FirstPageActivity extends AppCompatActivity {
         if (destination.equals("")) {
             Toast.makeText(getApplicationContext(), "도착지를 입력해주세요", Toast.LENGTH_SHORT).show();
         } else {
-            /* 비동기 처리하지 않고 우선 구현 */
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            /* 사용자가 검색한 출발점, 도착점 위도 경도 받아오기 */
-            TMapData tmapdata = new TMapData();
-            ArrayList<TMapPOIItem> destinationPOI = tmapdata.findAllPOI(destination);
-
             Intent main = new Intent(this, MainActivity.class);
-            main.putExtra("endLatitude", destinationPOI.get(0).frontLat);
-            main.putExtra("endLongitude", destinationPOI.get(0).frontLon);
-            String routeName = "현 위치에서 " + destination + "까지" + "\n";
+            main.putExtra("endLatitude", destinationPOI.get(userChosenOption).frontLat);
+            main.putExtra("endLongitude", destinationPOI.get(userChosenOption).frontLon);
+            String routeName = "현 위치에서 " + finalDestination + "까지" + "\n";
             Toast.makeText(getApplicationContext(), routeName + "경로 안내를 시작합니다", Toast.LENGTH_SHORT).show();
             startActivity(main);
         }
--- app/src/main/java/com/example/naviwavi/OptionActivity.java
+++ app/src/main/java/com/example/naviwavi/OptionActivity.java
@@ -4,28 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class OptionActivity extends AppCompatActivity {
+    public String oneName;
+    public String twoName;
+    public String threeName;
+    public String oneRoadName;
+    public String twoRoadName;
+    public String threeRoadName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
 
+        /* 부모 인텐트에서 받아오기 */
         Intent firstPageSetting = getIntent();
-        String oneName = firstPageSetting.getStringExtra("oneName");
-        String oneRoadName = firstPageSetting.getStringExtra("oneRoadName");
-        String twoName = firstPageSetting.getStringExtra("twoName");
-        String twoRoadName = firstPageSetting.getStringExtra("twoRoadName");
-        String threeName = firstPageSetting.getStringExtra("threeName");
-        String threeRoadName = firstPageSetting.getStringExtra("threeRoadName");
+        oneName = firstPageSetting.getStringExtra("oneName");
+        oneRoadName = firstPageSetting.getStringExtra("oneRoadName");
+        twoName = firstPageSetting.getStringExtra("twoName");
+        twoRoadName = firstPageSetting.getStringExtra("twoRoadName");
+        threeName = firstPageSetting.getStringExtra("threeName");
+        threeRoadName = firstPageSetting.getStringExtra("threeRoadName");
 
-        Button optionOne = (Button)findViewById(R.id.optionOne);
+        writeToButton();
+    }
+
+    private void writeToButton() {
+        Button optionOne = (Button) findViewById(R.id.optionOne);
         optionOne.setText(oneName + "\n" + oneRoadName);
-        Button optionTwo = (Button)findViewById(R.id.optionTwo);
-        optionOne.setText(twoName + "\n" + twoRoadName);
-        Button optionThree = (Button)findViewById(R.id.optionThree);
-        optionOne.setText(threeName + "\n" + threeRoadName);
+        Button optionTwo = (Button) findViewById(R.id.optionTwo);
+        optionTwo.setText(twoName + "\n" + twoRoadName);
+        Button optionThree = (Button) findViewById(R.id.optionThree);
+        optionThree.setText(threeName + "\n" + threeRoadName);
+    }
+
+    public void chooseOne(View v) {
+        FirstPageActivity.userChosenOption = 1;
+        FirstPageActivity.finalDestination = oneName;
+        finish();
+    }
+
+    public void chooseTwo(View v) {
+        FirstPageActivity.userChosenOption = 2;
+        FirstPageActivity.finalDestination = twoName;
+        finish();
+
+    }
+
+    public void chooseThree(View v) {
+        FirstPageActivity.userChosenOption = 3;
+        FirstPageActivity.finalDestination = threeName;
+        finish();
     }
 }
